2e89a9723522e8cbdcbcc8db2ba9a0d93006bc01
Riddhi Bagadiaa <43917903+RiddhiBagadiaa@users.noreply.github.com>
2022-09-06 12:31:28 -0700

    Lab 2 starter code

---
+++ Lab2/lab2/src/turtle_patrol/src/patrol_client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import numpy as np
+import rospy
+from turtle_patrol_sol.srv import Patrol  # Import service type
+
+
+def patrol_client():
+    # Initialize the client node
+    rospy.init_node('turtle1_patrol_client')
+    # Wait until patrol service is ready
+    rospy.wait_for_service('/turtle1/patrol')
+    try:
+        # Acquire service proxy
+        patrol_proxy = rospy.ServiceProxy(
+            '/turtle1/patrol', Patrol)
+        vel = 2.0  # Linear velocity
+        omega = 1.0  # Angular velocity
+        rospy.loginfo('Command turtle1 to patrol')
+        # Call patrol service via the proxy
+        patrol_proxy(vel, omega)
+    except rospy.ServiceException as e:
+        rospy.loginfo(e)
+
+
+if __name__ == '__main__':
+    patrol_client()
+
---
+++ Lab2/lab2/src/turtle_patrol/src/patrol_server.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from geometry_msgs.msg import Twist
+import numpy as np
+import rospy
+from std_srvs.srv import Empty
+from turtle_patrol_sol.srv import Patrol  # Service type
+from turtlesim.srv import TeleportAbsolute
+
+
+def patrol_callback(request):
+    rospy.wait_for_service('clear')
+    rospy.wait_for_service('/turtle1/teleport_absolute')
+    clear_proxy = rospy.ServiceProxy('clear', Empty)
+    teleport_proxy = rospy.ServiceProxy(
+        '/turtle1/teleport_absolute',
+        TeleportAbsolute
+    )
+    vel = request.vel  # Linear velocity
+    omega = request.omega  # Angular velocity
+    pub = rospy.Publisher(
+        '/turtle1/cmd_vel', Twist, queue_size=50)
+    cmd = Twist()
+    cmd.linear.x = vel
+    cmd.angular.z = omega
+    # Publish to cmd_vel at 5 Hz
+    rate = rospy.Rate(5)
+    # Teleport to initial pose
+    teleport_proxy(9, 5, np.pi/2)
+    # Clear historical path traces
+    clear_proxy()
+    while not rospy.is_shutdown():
+        pub.publish(cmd)  # Publish to cmd_vel
+        rate.sleep()  # Sleep until 
+    return cmd  # This line will never be reached
+
+def patrol_server():
+    # Initialize the server node for turtle1
+    rospy.init_node('turtle1_patrol_server')
+    # Register service
+    rospy.Service(
+        '/turtle1/patrol',  # Service name
+        Patrol,  # Service type
+        patrol_callback  # Service callback
+    )
+    rospy.loginfo('Running patrol server...')
+    rospy.spin() # Spin the node until Ctrl-C
+
+
+if __name__ == '__main__':
+    patrol_server()
+
